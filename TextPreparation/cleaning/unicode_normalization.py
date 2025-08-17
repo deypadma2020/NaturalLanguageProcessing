b898f54c478f7ba046574d0ca47ac00ed28d75a3
deypadma2020 <deypadma2020@gmail.com>
2025-08-17 13:14:02 +0530

    restructure folder

--- TextPreparation/cleaning/unicode_normalization.py
+++
@@ -1,8 +0,0 @@
-sample_text = """
-Café 😊 tastes better in the morning! 🌅  
-I ❤️ Python — it’s amazing! 🚀  
-Let’s meet at 5 pm for coffee ☕️
-"""
-
-clean_text = sample_text.encode("utf-8")
-print(clean_text)
